8c384d6319c1d679a5a5fac6e9e757115d4bdfd0
Deagen Stroup <deagenstroup@gmail.com>
2019-03-05 18:09:05 -0800

    Minor formatting changes. Added link box on home page. Changed about page

--- projects/models.py
+++ projects/models.py
@@ -5,5 +5,4 @@ class ProjectPage(models.Model):
     name = models.CharField(max_length=128, primary_key=True)
     title = models.CharField(max_length=1024)
     shortdesc = models.CharField(max_length=2048)
-    body = models.TextField()
 
